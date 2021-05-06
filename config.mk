dd61635cc8709ae3ce2b442b5f8da9cfb95b425c
durgesh33 <dgagoria33@gmail.com>
2021-05-06 10:57:14 +0530

    oneplus_apps: Add OnePlusFileManager

--- config.mk
+++ config.mk
@@ -1,6 +1,11 @@
 PRODUCT_PACKAGES += \
     OnePlusScreenRecorder \
-    OnePlusGallery
+    OnePlusGallery \
+	OnePlusFileManager
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-filemanager.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-filemanager.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml
